03599c5c9f1ba08985b4c621045a42fc4b732a03
byeond009 <1249413181@qq.com>
2023-03-30 22:25:43 +0800

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div></div>;
+  return <div className="text-8xl">TODO </div>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.html", "./src/**/*.{react,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
